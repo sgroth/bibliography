b0f2b77f05585ff263f04e65999fe539432b6efb
sgroth <stefangroth@me.com>
2017-09-11 16:12:40 +0200

    updated bib data

--- bib-generate.sh
+++ bib-generate.sh
@@ -16,7 +16,7 @@ mkdir markdown
 chmod 777 markdown
 # create en.bib and replace strings
 cp sgroth-bibliography.bib sgroth-bibliography.en.bib
-sed -i '' -e 's/Zur[[:space:]]Publikation[[:space:]]eingereicht/Submitted for publication/g; s/Rezension[[:space:]]von/Review of/g; s/herausgegeben[[:space:]]von/edited by/g; s/} von/} by/g; s/Im[[:space:]]Druck/In print/g; s/Sondernummer/special issue/g' sgroth-bibliography.en.bib
+sed -i '' -e 's/Zur[[:space:]]Publikation[[:space:]]eingereicht/Submitted for publication/g; s/Rezension[[:space:]]von/Review of/g; s/herausgegeben[[:space:]]von/edited by/g; s/} von/} by/g; s/Im[[:space:]]Druck/In print/g; s/Sondernummer/special issue/g; s/Zur[[:space:]]Publikation[[:space:]]angenommen/Accepted for publication/g' sgroth-bibliography.en.bib
 # run latexmk
 latexmk -pdf publications.de.tex
 latexmk -pdf publications.en.tex
