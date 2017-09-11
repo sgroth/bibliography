ac21cf22837d93512b72f0219d1ba82bd4c58122
sgroth <stefangroth@me.com>
2017-09-11 10:14:02 +0200

    updated bib data

--- bib-generate.sh
+++ bib-generate.sh
@@ -6,10 +6,15 @@ git commit -m "updated bib data"
 git push origin master
 # get vc info (pythonw)
 pythonw gfi-run.py
+# delete contents of pdf
+rm -rf pdf
+# mkdir
+mkdir pdf
+chmod 777 pdf
 # run latexmk
 latexmk -pdf publications.de.tex
 # delete aux-files
-latexmk -CA publications.de.tex
+# latexmk -CA publications.de.tex
 # move pdf to subdirectory
 mv publications.de.pdf pdf/
 # add and commit to git (PDF result)
