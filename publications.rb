2e32931660a676975d1aa6accdb2301a3054745f
sgroth <stefangroth@me.com>
2019-04-02 14:03:00 +0200

    updated bib data

--- publications.rb
+++ publications.rb
@@ -1,3 +1,4 @@
+# Generate Markdown-Version of Publications
 #!/usr/bin/env ruby'
 require 'bibtex'
 require 'citeproc'
@@ -19,60 +20,59 @@ bib = BibTeX.open('sgroth-bibliography.bib').convert(:latex2,:latex)
 
 CSL::Style.root = "/Users/stefangroth/.csl/"
 
-
 # Create a new processor with the desired style, # format, and locale.
-cp = CiteProc::Processor.new style: 'Unknown', format: 'text', locale: 'de'
+cp = CiteProc::Processor.new style: 'unknown', format: 'text', locale: 'de'
 
-sortorder = cp.engine.style.bibliography > 'sort' > 'key'
-puts sortorder
+# sortorder = cp.engine.style.bibliography > 'sort' > 'key'
+# puts sortorder
 # sortorder[:macro] = 'issued'
 # sortorder[:sort] = 'descending'
 # puts sortorder
 
 cp.import bib.to_citeproc
 
-# Process entries with keyword "submitted"
-submitted = bib['@*[keywords=submitted]'].map do |e|
-  "* #{cp.render(:bibliography, id: e.key)[0]}"
-end
-
 # Process entries with keyword "inpreparation"
-inpreparation = bib['@*[keywords=inpreparation]'].map do |e|
+inpreparation = bib['@*[keywords~=inpreparation]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process entries with keyword "monography"
-monography = bib['@*[keywords=monography]'].map do |e|
+monography = bib['@book[keywords!=notaccepted  && keywords!=miszelle &&  keywords!=inpreparation && keywords=monography]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process entries with keyword "editedvolume"
-editedvolume = bib['@*[keywords=editedvolume]'].map do |e|
+editedvolume = bib['@*[keywords ^= editedvolume && keywords !~ notaccepted && keywords!~inpreparation]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process entries with keyword "specialissue"
-specialissue = bib['@*[keywords=specialissue]'].map do |e|
+specialissue = bib['@periodical'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
-# Process articles
-article = bib['@article[keywords/=submitted]'].map do |e|
+# Process peer reviewed articles
+peerreviewedarticle = bib['@article[keywords!~ notaccepted && keywords^=peerreview && keywords!~inpreparation]'].reverse_each.map do |e|
+  "* #{cp.render(:bibliography, id: e.key)[0]}"
+end
+
+# Process other articles
+article = bib['@article[ keywords!~ peerreview && keywords!~ notaccepted && keywords!~inpreparation]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process incollection
-incollection = bib['@incollection[keywords!=submitted]'].map do |e|
+incollection = bib['@incollection[keywords!~ inpreparation && keywords!=miszelle && keywords!=notaccepted]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process entries with keyword "miszelle"
-miszelle = bib['@*[keywords=miszelle]'].map do |e|
+miszelle = bib['@*[keywords=miszelle]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
 # Process entries with keyword "review"
-review = bib['@review[keywords/=submitted]'].map do |e|
+review = bib['@review[keywords!=dev]'].reverse_each.map do |e|
   "* #{cp.render(:bibliography, id: e.key)[0]}"
 end
 
@@ -80,11 +80,9 @@ date = Time.new
 current_time = date.year.to_s + "-" + date.month.to_s + "-" + date.day.to_s 
 
 # Write to output file
-File.open( 'publications.de.markdown', "w" ) do |file|
+File.open( 'publications.de.md', "w" ) do |file|
   file.puts "# Schriftenverzeichnis\nDr. Stefan Groth  \n[https://www.stefangroth.com](https://www.stefangroth.com)\n\n"
-  file.puts "Version: " + current_time + "\n\n## Eingereichte Beiträge"
-  file.puts submitted.join("  \n")
-  file.puts "\n## Beiträge in Vorbereitung"
+  file.puts "Version: " + current_time + "\n\n## Beiträge im Erscheinen und in Vorbereitung"
   file.puts inpreparation.join("  \n")
   file.puts "\n## Monographien"
   file.puts monography.join("  \n")
@@ -92,6 +90,8 @@ File.open( 'publications.de.markdown', "w" ) do |file|
   file.puts editedvolume.join("  \n")
   file.puts "\n## Sondernummern von Zeitschriften"
   file.puts specialissue.join("  \n")
+  file.puts "\n## Peer Reviewed Papers"
+  file.puts peerreviewedarticle.join("  \n")
   file.puts "\n## Zeitschriftenaufsätze"
   file.puts article.join("  \n")
   file.puts "\n## Aufsätze in Sammelbänden"
@@ -101,4 +101,3 @@ File.open( 'publications.de.markdown', "w" ) do |file|
   file.puts "\n## Rezensionen"
   file.puts review.join("  \n")
 end
-puts "Created publications.de.markdown"
